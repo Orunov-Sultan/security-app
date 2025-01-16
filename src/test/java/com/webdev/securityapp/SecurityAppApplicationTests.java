b02a047fac992482433627b2f9790c76b3eb1934
Sultan <osultan@yandex.ru>
2025-01-16 12:00:05 +0500

    Initial commit

---
+++ src/test/java/com/webdev/securityapp/SecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
